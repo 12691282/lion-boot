f90ef96461068c5128b26ee156d0a5f23b832fac
12691282 <12691282@qq.com>
2019-09-07 18:19:07 +0800

    config init data

--- src/main/java/com/alpha/core/tools/ResultObject.java
+++ src/main/java/com/alpha/core/tools/ResultObject.java
@@ -9,6 +9,12 @@ public class ResultObject<T> implements Serializable {
 
     private static final String success_code = "000";
 
+    private static final String success_default_msg = "操作成功";
+
+    private static final String defeat_code = "001";
+
+    private static final String defeat_default_msg = "数据读取错误";
+
     //返回状态 成功 000 失败 001
     private String code;
     //消息提示
@@ -26,20 +32,33 @@ public class ResultObject<T> implements Serializable {
     public ResultObject(String code, T data){
         this.code = code;
         this.data = data;
+
     }
 
-
+    public ResultObject(String code, String msg, T data){
+        this.code = code;
+        this.data = data;
+        this.message = msg;
+    }
 
     public static ResultObject getSuccess(){
         return new ResultObject(success_code);
     }
 
     public static <T> ResultObject getSuccess(T data){
-        return new ResultObject(success_code, data);
+        return new ResultObject(success_code, success_default_msg, data);
     }
 
     public static <T> ResultObject  getSuccess(String message,T data){
         return new ResultObject(message, data);
     }
 
+    public static ResultObject getDefeat()
+    {
+        return new ResultObject(defeat_code, defeat_default_msg);
+    }
+    public static ResultObject getDefeat(String msg)
+    {
+        return new ResultObject(defeat_code, msg);
+    }
 }
