72846fdedb5ed10804b7a26aee2d52e4b8a366ed
12691282 <12691282@qq.com>
2019-09-30 19:42:55 +0800

    add filter

---
+++ src/main/java/com/alpha/core/config/WebInfoConfig.java
@@ -0,0 +1,76 @@
+package com.alpha.core.config;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class WebInfoConfig implements WebMvcConfigurer {
+
+    /*********************
+     * 拦截器设置
+     * *************************/
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 拦截所有请求
+        registry.addInterceptor(apiParameterInterceptor()).addPathPatterns("/**");
+    }
+
+
+    @Bean
+    public ApiParameterInterceptor apiParameterInterceptor() {
+        return new ApiParameterInterceptor();
+    }
+
+
+
+    @Bean
+    public JsonParamsFilter memberFilter() {
+        return new JsonParamsFilter();
+    }
+
+    /****************************
+     * 跨域请求设置
+     * *****************************/
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //设置允许跨域的路径
+        registry.addMapping("/**")
+                //设置允许跨域请求的域名
+                .allowedOrigins("*")
+                //是否允许证书 不再默认开启
+                .allowCredentials(true)
+                //设置允许的方法
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                //跨域允许时间
+                .maxAge(3600);
+    }
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");// 1允许任何域名使用
+        config.addAllowedHeader("*");// 2允许任何头
+        config.addAllowedMethod("GET");// 3允许任何方法（post、get等）
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+
+}
