4ef11db614c5001da85884cc9c8852a8b3adb20d
ext.liqiang8 <ext.liqiang8@jd.com>
2022-03-02 14:57:47 +0800

    test

--- src/test/java/com.alpha.module.system.service/TestAccountService.java
+++ src/test/java/com.alpha.module.system.service/TestAccountService.java
@@ -1,8 +1,12 @@
 package com.alpha.module.system.service;
 
 import com.alpha.MainApplication;
+import com.alpha.core.tools.PageTools;
+import com.alpha.module.system.model.AccountModel;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,4 +14,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MainApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestAccountService {
+
+
+    @Autowired
+    private AccountService accountService;
+
+
+    @Test
+    public void test(){
+        AccountModel query = new AccountModel();
+        PageTools page = accountService.getList(query);
+        log.info("page {}", page);
+    }
+
+
 }
